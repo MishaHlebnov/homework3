8592afe675870cdbaf8d26c8d43726e464b48ece
Misha <mishaglebnov99@gmail.com>
2021-10-20 20:26:26 +0500

    completed the second task

--- src/main/java/homework3.java
+++ src/main/java/homework3.java
@@ -1,6 +1,13 @@
 public class homework3 {
     public static void main(String[] args) {
-        exercise1();
+        exercise2();
+    }
+    public static void exercise2() {
+        int[] a = new int[100];
+        for (int i = 0; i < a.length; i++) {
+            a[i]= i + 1;
+            System.out.println(a[i]);
+        }
     }
     public static void exercise1() {
         int[] a = {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1};
